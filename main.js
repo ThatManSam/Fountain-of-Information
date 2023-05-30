77b7994b509e7c52f8e12a384a19b4144327ee4d
yereeem <132246494+yereeem@users.noreply.github.com>
2023-05-30 12:35:22 +1200

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,51 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { Fountain } from './src/fountain'
 
+let list;
+let item;
+
+fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=-37.8&lon=175.3&units=metric&lang=en', {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '44c686a889msh9cfed311931649bp1e2430jsn02f6698a3868',
+		'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
+	}
+})
+
+	.then(response => response.json())
+	.then(data => {
+		list = data.data;
+
+		list.map((item) => {
+			console.log(item) // See all the item.properties.
+			const time = item.timestamp_local;
+			const velocity = item.app_temp;
+			const description = item.weather.description;
+			const windSpeed = item.wind_spd;
+
+			// Array to loop through fountain values.
+
+			times.push(time);
+			velocities.push(velocity);
+			descriptions.push(description);
+			windSpeeds.push(windSpeed);
+
+			// Shows time as the heading, and then lists (horizontally) the other listed properties.. Hopefully..
+			// const curWeather = `<li><h1>${time}</h1><h2>${weather}</h2><h3>${temperature}</h3><h4>${windSpeed}</h4></li>`
+			// document.querySelector('.weather').innerHTML += curWeather;
+		})
+	})
+
+	.catch(err => {
+		console.error(err);
+	});
+
+// 
+let times = []
+let velocities = []
+let descriptions = []
+let windSpeeds = []
+
 let controls
 let scene
 let camera
@@ -84,6 +129,14 @@ function animateFountain(timeElapsed) {
 	fountain1.Step(timeElapsedS)
 	fountain2.Step(timeElapsedS)
 	fountain3.Step(timeElapsedS)
+	
+	fountain1.velocity = velocities[0]
+	fountain2.velocity = velocities[1]
+	fountain3.velocity = velocities[2]
+
+	fountain1.spread = windSpeeds[0]
+	fountain2.spread = windSpeeds[1]
+	fountain3.spread = windSpeeds[2]
 }
 
 Init()
