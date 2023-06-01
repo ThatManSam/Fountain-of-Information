faea63b9cc3f4a1dfc0b999682196f40e41760a5
hans <111059193+1206hanex@users.noreply.github.com>
2023-06-01 22:44:36 +1200

    Update main.js
    Added support for GLTF 3D objects.
    Added text to provide info for the fountains (WIP).
    Added the ability to change colours of the fountains (base colour only, still fades to white, still a WIP).
    
    Removed OrbitControls (it messes with the fixed camera).

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js'
 import { Fountain } from './src/fountain'
 
 let list
+let times = []
+let velocities = []
+let descriptions = []
+let windSpeeds = []
 
+let scene
+let camera
+let renderer
+let labelRenderer
+let fountain1, fountain2, fountain3
+let f1, f2, f3
+let env
+
+// fetch data from weather api
 function call() {
 	fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=-37.8&lon=175.3&units=metric&lang=en', {
 		method: 'GET',
@@ -38,23 +52,10 @@ call()
 // call api every 30 mins
 setTimeout(call, 1800000)
 
-let times = []
-let velocities = []
-let descriptions = []
-let windSpeeds = []
-
-let controls
-let scene
-let camera
-let renderer
-let fountain1
-let fountain2
-let fountain3
-let spread = 1.0
-
 function Init() {
 	scene = new THREE.Scene()
 
+	// initialise WebGL renderer
 	renderer = new THREE.WebGLRenderer({
 		canvas: document.querySelector('#bg'),
 		antialias: true
@@ -62,42 +63,80 @@ function Init() {
 	renderer.setPixelRatio(window.devicePixelRatio)
 	renderer.setSize(window.innerWidth, window.innerHeight)
 	document.body.appendChild(renderer.domElement)
+	
+	// initialise HTML elements renderer
+	labelRenderer = new CSS2DRenderer()
+	labelRenderer.setSize(window.innerWidth, window.innerHeight)
+	labelRenderer.domElement.style.position = 'absolute'
+	labelRenderer.domElement.style.top = '0px'
+	document.body.appendChild(labelRenderer.domElement)
 
+	// initialise camera to a fixed point
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1.0, 1000.0)
-	camera.position.set(25, 10, 0)
+	camera.position.set(30, 15, 0)
+	camera.lookAt(0, 14, 0)
 
-	const grid = new THREE.GridHelper(200, 50)
-	scene.add(grid)
+	const pointLight = new THREE.PointLight(0xFFFFFF)
+	pointLight.position.set(0, 50, 0)
+	scene.add(pointLight)
+	
+	const bg = new THREE.TextureLoader().load('./resources/bg1.png')
+	scene.background = bg
 
-	controls = new OrbitControls(camera, renderer.domElement)
-	controls.update()
-
-	const light = new THREE.AmbientLight(0x101010);
-	scene.add(light);
-
+	// initialise fountains
 	fountain1 = new Fountain({
 		scene: scene,
 		camera: camera,
 		velocity: 0,
-		spread: spread,
-		positionX: 50,
+		colour: new THREE.Color(0x6a72de),
+		spread: 1.0,
+		positionZ: 20,
 	})
 
 	fountain2 = new Fountain({
 		scene: scene,
 		camera: camera,
 		velocity: 0,
-		spread: spread,
-		positionX: 0,
+		colour: new THREE.Color(0x6ade70),
+		spread: 1.0,
+		positionZ: 0,
 	})
 
 	fountain3 = new Fountain({
 		scene: scene,
 		camera: camera,
 		velocity: 0,
-		spread: spread,
-		positionX: -50,
+		colour: new THREE.Color(0xde6ad0),
+		spread: 1.0,
+		positionZ: -20,
 	})
+	
+	// load 3D environment
+	env = new GLTFLoader()
+	env.load('./resources/eng.glb', function(glb){
+		const model = glb.scene
+		scene.add(model)
+		model.matrixAutoUpdate = false
+	})
+	
+	// create labels for the fountains
+	f1 = document.createElement('f1')
+	f1.textContent = 'Text'
+	const cPointLabel1 = new CSS2DObject(f1)
+	scene.add(cPointLabel1)
+	cPointLabel1.position.set(2, 0, 20)
+
+	f2 = document.createElement('f2')
+	f2.textContent = 'Text'
+	const cPointLabel2 = new CSS2DObject(f2)
+	scene.add(cPointLabel2)
+	cPointLabel2.position.set(2, 0, 0)
+
+	f3 = document.createElement('f3')
+	f3.textContent = 'Text'
+	const cPointLabel3 = new CSS2DObject(f3)
+	scene.add(cPointLabel3)
+	cPointLabel3.position.set(2, 0, -20)
 
 	renderer.render(scene, camera)
 }
